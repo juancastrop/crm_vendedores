97670dcd0e30c5af53a70f198c5f82d5275e848f
juan <jfcastropalma@gmail.com>
2023-07-11 19:17:10 -0400

    agregando productos y categorias con bases relacionales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
     include Pagy::Backend
     Pagy::DEFAULT[:items] = 5
+
+    def authorize_request(kind = nil)
+        unless kind.include?(current_user.role)
+        redirect_to posts_path, notice: "You are not authorized to perform this action"
+        end
+    end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,17 @@
 class WelcomeController < ApplicationController
+  
+  #usuario normal
+  before_action only: [:index, :show] do
+    authorize_request(["normal","ejecutivo","admin"])
+  end
+  #usuario ejecutivo
+  before_action only: [:new, :edit, :create, :update] do
+    authorize_request(["ejecutivo","admin"])
+  end
+  #usuario admin
+  before_action only: [:destroy] do
+    authorize_request(["admin"])
+  end
   def index
   end
 end
